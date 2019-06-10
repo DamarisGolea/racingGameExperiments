64194dfe31cbc9d27b2773b7f20e650fc9f63b2d
Your Name <damaris.golea@gmail.com>
2019-06-10 14:50:58 +0300

    set-up basic project structure

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,43 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Car car = new Car();
+        car.name = "Dacia";
+        car. color = "blue";
+        car.doorCount = 4;
+        car. mileage = 9.5;
+        car. running = true;
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+
+        car.engine = engine;
+
+        System.out.println("Our engine...");
+        System.out.println(car.engine.manufacturer);
+
+
+        Car car2 = new Car();
+        car2.name = "Volvo";
+        car2.color = "black";
+        car2.doorCount = 4;
+        car2.mileage = 8.3;
+        car2.running= false;
+
+        Car car3 = car;
+
+        System.out.println(car.name);
+        System.out.println(car3.name);
+
+        System.out.println("Changing values...");
+
+        car.name = "Dacia Logan";
+
+        System.out.println(car.name);
+        System.out.println(car3.name);
+
+        System.out.println(car.maxSpeed);
+        System.out.println(car.engine);
+
     }
 }
